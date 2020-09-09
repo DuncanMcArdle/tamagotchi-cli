c1d163246683f27c4e44fdd01273cb58da2a44ef
Duncan McArdle <duncan.mcardle@isosec.co.uk>
2020-09-09 15:17:20 +0100

    Added basic class for tamagotchi

---
+++ src/classes/tamagotchi.class.ts
@@ -0,0 +1,20 @@
+import { clear } from 'console';
+
+export default class Tamagotchi {
+	birth: Date;
+	food: number;
+	maxFood: number;
+	age: number;
+	maxAge: number;
+	alive: boolean;
+
+	// Constructor
+	constructor(birth: Date, food: number) {
+		this.birth = birth;
+		this.food = food;
+		this.maxFood = 10;
+		this.age = 0;
+		this.maxAge = 10;
+		this.alive = true;
+	}
+}
